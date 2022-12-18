5f0adcc08c75d2be5f9b17e980984c0211c4f04e
JiroKaya <jiro.kaya.ch@gmail.com>
2022-12-18 12:06:45 +0100

    [change] Updated to CRUD artist and user

--- src/ProductController.php
+++ src/ProductController.php
@@ -17,22 +17,22 @@ class ProductController
 
     private function processResourceRequest(string $method, string $endpoint, string $id): void
     {
-        $artist = $this->gateway->get($id, $endpoint);
+        $search_object = $this->gateway->get($id, $endpoint);
 
-        if (!$artist) {
+        if (!$search_object) {
             http_response_code(404);
-            echo json_encode(["message" => "Artist not found"]);
+            echo json_encode(["message" => "Object of search was not found"]);
             return;
         }
 
         switch ($method) {
             case "GET":
-                echo json_encode($artist);
+                echo json_encode($search_object);
                 break;
             case "PATCH":
                 $data = (array) json_decode(file_get_contents("php://input"), true);
 
-                $errors = $this->getValidationErrors($data, false);
+                $errors = $this->getValidationErrors($data, $endpoint, false);
 
                 if (!empty($errors)) {
                     http_response_code(422);
@@ -40,17 +40,32 @@ class ProductController
                     break;
                 }
 
-                $rows = $this->gateway->update($artist, $data);
-                json_encode([
-                    "message" => "Artist $id updated",
-                    "rows" => $rows
-                ]);
+                switch ($endpoint) {
+                    case 'users':
+                        $rows = $this->gateway->update_user($search_object, $data);
+                        echo json_encode([
+                            "message" => "User $id updated",
+                            "rows" => $rows
+                        ]);
+                        break;
+                    case 'artists':
+                        $rows = $this->gateway->update_artist($search_object, $data);
+                        echo json_encode([
+                            "message" => "Artist $id updated",
+                            "rows" => $rows
+                        ]);
+                        break;
+                    default:
+                        http_response_code(412);
+                        header("Only users and artists allowed");
+                        break;
+                }
                 break;
             case "DELETE":
-                $rows = $this->gateway->delete($id);
+                $rows = $this->gateway->delete($id, $endpoint);
 
                 echo json_encode([
-                    "message" => "Artist $id deleted",
+                    "message" => "$endpoint $id deleted",
                     "rows" => $rows
                 ]);
                 break;
@@ -58,20 +73,18 @@ class ProductController
                 http_response_code(405);
                 header("Allow: GET, PATCH, DELETE");
         }
-
-        echo json_encode($artist);
     }
 
-    private function processCollectionRequest(string $method): void
+    private function processCollectionRequest(string $method, string $endpoint): void
     {
         switch ($method) {
             case "GET":
-                echo json_encode($this->gateway->getAll());
+                echo json_encode($this->gateway->getAll($endpoint));
                 break;
             case "POST":
                 $data = (array) json_decode(file_get_contents("php://input"), true);
 
-                $errors = $this->getValidationErrors($data);
+                $errors = $this->getValidationErrors($data, $endpoint);
 
                 if (!empty($errors)) {
                     http_response_code(422);
@@ -79,38 +92,95 @@ class ProductController
                     break;
                 }
 
-                $id = $this->gateway->create($data);
+                switch ($endpoint) {
+                    case 'users':
+                        $id = $this->gateway->create_user($data);
 
-                http_response_code(201);
-                json_encode([
-                    "message" => "User created",
-                    "id" => $id
-                ]);
-                break;
+                        http_response_code(201);
+                        echo json_encode([
+                            "message" => "User created",
+                            "id" => $id
+                        ]);
+                        break;
+                    case 'artists':
+                        $id = $this->gateway->create_artist($data);
+
+                        http_response_code(201);
+                        echo json_encode([
+                            "message" => "Artist created",
+                            "id" => $id
+                        ]);
+                        break;
+                    default:
+                        http_response_code(412);
+                        header("Only users and artists allowed");
+                        break;
+                }
             default:
                 http_response_code(405);
                 header("Allow: GET, POST");
         }
     }
 
-    private function getValidationErrors (array $data, bool $is_new = true): array
+    private function getValidationErrors (array $data, string $endpoint, bool $is_new = true): array
     {
         $errors = [];
 
-        if ($is_new && empty($data["username"])) {
-            $errors[] = "username is required";
+        switch ($endpoint) {
+            case 'users':
+                if ($is_new && empty($data["username"])) {
+                    $errors[] = "username is required";
+                }
+        
+                if ($is_new && empty($data["password"])) {
+                    $errors[] = "password is required";
+                }
+        
+                if ($is_new && empty($data["created"])) {
+                    $errors[] = "time of creation is required";
+                }
+
+                if ($is_new && empty($data["modified"])) {
+                    $errors[] = "time of modification is required";
+                }
+        
+                if ($is_new && empty($data["high_score"])) {
+                    $errors[] = "highscore is required";
+                }
+                break;
+            case 'artists':
+                if ($is_new && empty($data["spotify_id"])) {
+                    $errors[] = "username is required";
+                }
+        
+                if ($is_new && empty($data["name"])) {
+                    $errors[] = "password is required";
+                }
+        
+                if ($is_new && empty($data["created"])) {
+                    $errors[] = "time of creation is required";
+                }
+
+                if ($is_new && empty($data["modified"])) {
+                    $errors[] = "time of modification is required";
+                }
+        
+                if ($is_new && empty($data["pop_score"])) {
+                    $errors[] = "highscore is required";
+                }
+                break;
+            default:
+                http_response_code(412);
+                header("Only users and artists allowed");
+                break;
         }
 
-        if ($is_new && empty($data["password"])) {
-            $errors[] = "password is required";
+        if ($is_new == false && empty($data["modified"])) {
+            $errors[] = "time of modification is required when updating";
         }
 
-        if ($is_new && empty($data["created"])) {
-            $errors[] = "time of creation is required";
-        }
-
-        if ($is_new && empty($data["high_score"])) {
-            $errors[] = "highscore is required";
+        if ($is_new == false && empty($data["created"]) == false) {
+            $errors[] = "changing time of creation when updating is not allowed";
         }
 
         return $errors;
--- src/ProductGateway.php
+++ src/ProductGateway.php
@@ -8,11 +8,25 @@ class ProductGateway
         $this->conn = $database->getConnection();    
     }
 
-    public function getAll(): array
+    public function getAll(string $endpoint): array
     {
-        $sql = "SELECT * FROM artist_data";
-
-        $stmt = $this->conn->query($sql);
+        switch ($endpoint) {
+            case 'users':
+                $table = "users";
+                break;
+            case 'artists':
+                $table = "artist_data";
+                break;
+            default:
+                http_response_code(412);
+                header("Only users and artists allowed");
+                break;
+        }
+        
+        $sql = "SELECT * FROM `$table`";
+        
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
 
         $data = [];
 
@@ -27,14 +41,43 @@ class ProductGateway
         return $data;
     }
 
-    public function create(array $data): string
+    public function create_artist(array $data)
     {
-        $sql = "INSERT INTO users (username, password, created, high_score) VALUES (:username, :password, :created, :high_score)";
+        $sql = "INSERT INTO artist_data 
+        (spotify_id, name, img_link, genre1, genre2, pop_score, followers, created, modified) 
+        VALUES (:spotify_id, :name, :img_link, :genre1, :genre2, :pop_score, :followers, :created, :modified)";
+
         $stmt = $this->conn->prepare($sql);
 
-        $stmt->bindValue(":username", $data["username"], PDO::PARAM_STR);
-        $stmt->bindValue(":password", $data["password"], PDO::PARAM_STR);
+        $stmt->bindValue(":spotify_id", $data["spotify_id"], PDO::PARAM_STR);
+        $stmt->bindValue(":img_link", $data["img_link"], PDO::PARAM_STR);
+        $stmt->bindValue(":genre1", $data["genre1"], PDO::PARAM_STR);
+        $stmt->bindValue(":genre2", $data["genre2"], PDO::PARAM_STR);
+        $stmt->bindValue(":pop_score", $data["pop_score"], PDO::PARAM_INT);
+        $stmt->bindValue(":followers", $data["followers"], PDO::PARAM_INT);
         $stmt->bindValue(":created", date("Y-m-d H:i:s",strtotime($data["created"])), PDO::PARAM_STR);
+        $stmt->bindValue(":modified", date("Y-m-d H:i:s",strtotime($data["modified"])), PDO::PARAM_STR);
+        $stmt->bindValue(":high_score", $data["high_score"], PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        return $this->conn->lastInsertId();
+    }
+
+    public function create_user(array $data): string
+    {
+        $sql = "INSERT INTO users 
+        (username, password, created, modified, high_score) 
+        VALUES (:username, :password, :created, :modified, :high_score)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $hashed_password = password_hash($data["password"], PASSWORD_DEFAULT);
+
+        $stmt->bindValue(":username", $data["username"], PDO::PARAM_STR);
+        $stmt->bindValue(":password", $hashed_password, PDO::PARAM_STR);
+        $stmt->bindValue(":created", date("Y-m-d H:i:s",strtotime($data["created"])), PDO::PARAM_STR);
+        $stmt->bindValue(":modified", date("Y-m-d H:i:s",strtotime($data["modified"])), PDO::PARAM_STR);
         $stmt->bindValue(":high_score", $data["high_score"], PDO::PARAM_INT);
 
         $stmt->execute();
@@ -44,21 +87,24 @@ class ProductGateway
 
     public function get(string $id, string $endpoint): array | false
     {
-
-        $sql = "SELECT * FROM :db WHERE id = :id";
-        
-        $stmt = $this->conn->prepare($sql);
-
-        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
-
         switch($endpoint) {
             case "users":
-                $stmt->bindValue(":db", "users", PDO::PARAM_INT);
+                $table = "users";
+                break;
             case "artists":
-                $stmt->bindValue(":db", "artist_data", PDO::PARAM_INT);
+                $table = "artist_data";
+                break;
+            default:
+                throw new Exception("Only users and artists allowed");
+                break;
         }
-
+        
+        $sql = "SELECT * FROM `$table` WHERE id = :id";
+        
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
         $stmt->execute();
+        
 
         $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -70,20 +116,18 @@ class ProductGateway
         return $data;
     }
 
-    public function update(array $current, array $new): int
+    public function update_user(array $current, array $new): int
     {
-        $sql = "UPDATE :db SET spotify_id=:spotify_id, name=:name, img_link=:img_link, genre1=:genre1, genre2=:genre2, pop_score=:pop:score, followers=:followers, modified=:modified WHERE id = :id";
+        $sql = "UPDATE users 
+        SET username=:username, password=:password, modified=:modified, high_score=:high_score 
+        WHERE id = :id";
 
         $stmt = $this->conn->prepare($sql);
 
-        $stmt->bindValue(":spotify_id", $new["spotify_id"] ?? $current["spotify_id"], PDO::PARAM_STR);
-        $stmt->bindValue(":name", $new["name"] ?? $current["name"], PDO::PARAM_STR);
-        $stmt->bindValue(":img_link", $new["img_link"] ?? $current["img_link"], PDO::PARAM_STR);
-        $stmt->bindValue(":genre1", $new["genre1"] ?? $current["genre1"], PDO::PARAM_STR);
-        $stmt->bindValue(":genre2", $new["genre2"] ?? $current["genre2"], PDO::PARAM_STR);
-        $stmt->bindValue(":pop_score", $new["pop_score"] ?? $current["pop_score"], PDO::PARAM_STR);
-        $stmt->bindValue(":followers", $new["followers"] ?? $current["followers"], PDO::PARAM_STR);
-        $stmt->bindValue(":modified", $new["modified"] ?? $current["modified"], PDO::PARAM_STR);
+        $stmt->bindValue(":username", $new["username"] ?? $current["username"], PDO::PARAM_STR);
+        $stmt->bindValue(":password", $new["password"] ?? $current["password"], PDO::PARAM_STR);
+        $stmt->bindValue(":modified", date("Y-m-d H:i:s",strtotime($new["modified"])) ?? $current["modified"], PDO::PARAM_STR);
+        $stmt->bindValue(":high_score", $new["high_score"] ?? $current["high_score"], PDO::PARAM_STR);
         
         $stmt->bindValue(":id", $current["id"], PDO::PARAM_INT);
         
@@ -92,12 +136,47 @@ class ProductGateway
         return $stmt->rowCount();
     }
 
-    public function delete(string $id): int
+    public function update_artist(array $current, array $new): int
     {
-        $sql = "DELETE FROM artist_data WHERE id = :id";
+        $sql = "UPDATE artist_data
+        SET spotify_id=:spotify_id, name=:name, img_link=:img_link, genre1=:genre1, genre2=:genre2, pop_score=:pop_score, followers=:followers, modified=:modified 
+        WHERE id = :id";
 
         $stmt = $this->conn->prepare($sql);
 
+        $stmt->bindValue(":spotify_id", $new["spotify_id"] ?? $current["spotify_id"], PDO::PARAM_STR);
+        $stmt->bindValue(":name", $new["name"] ?? $current["name"], PDO::PARAM_STR);
+        $stmt->bindValue(":img_link", $new["img_link"] ?? $current["img_link"], PDO::PARAM_STR);
+        $stmt->bindValue(":genre1", $new["genre1"] ?? $current["genre1"], PDO::PARAM_STR);
+        $stmt->bindValue(":genre2", $new["genre2"] ?? $current["genre2"], PDO::PARAM_STR);
+        $stmt->bindValue(":pop_score", $new["pop_score"] ?? $current["pop_score"], PDO::PARAM_INT);
+        $stmt->bindValue(":followers", $new["followers"] ?? $current["followers"], PDO::PARAM_STR);
+        $stmt->bindValue(":modified", date("Y-m-d H:i:s",strtotime($new["modified"])) ?? $current["modified"], PDO::PARAM_STR);
+        
+        $stmt->bindValue(":id", $current["id"], PDO::PARAM_INT);
+        
+        $stmt->execute();
+
+        return $stmt->rowCount();
+    }
+
+    public function delete(string $id, string $endpoint): int
+    {
+        $sql = "DELETE FROM :table WHERE id = :id";
+
+        $stmt = $this->conn->prepare($sql);
+
+        switch ($endpoint) {
+            case 'users':
+                $stmt->bindValue(":table", "users", PDO::PARAM_STR);
+                break;
+            case 'artist':
+                $stmt->bindValue(":table", "artist_data", PDO::PARAM_STR);
+            default:
+                http_response_code(412);
+                header("Only users and artists allowed");
+                break;
+        }
         $stmt->bindValue(":id", $id, PDO::PARAM_INT);
 
         $stmt->execute();
